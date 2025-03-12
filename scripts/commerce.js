3542e523536d3163b77aa1f851916e656dbb7487
mfren114 <mathew.frenette@yahoo.com>
2025-03-12 11:12:15 -0700

    update

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -1,3 +1,4 @@
+//current date
 const currentYear = new Date().getFullYear();
 
 document.getElementById("currentYear").textContent = currentYear;
@@ -5,3 +6,13 @@ document.getElementById("currentYear").textContent = currentYear;
 alert(document.lastModified);
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
+
+//hamburger button
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
