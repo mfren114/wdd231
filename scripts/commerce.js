3c091b5dcaa51ec4958c60316557abd8104cc6c2
mfren114 <mathew.frenette@yahoo.com>
2025-03-12 14:25:51 -0700

    update

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -16,3 +16,51 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+//Courses
+
+const courses = [
+    {
+        subject: "CSE",
+        number: 110,
+        title: "Introduction to Programming",
+        credits: 2,
+        complete: true
+    },
+    {
+        subject: "WDD",
+        number: 130,
+        title: "Web Fundamentals",
+        credits: 2,
+        complete: true
+    },
+    {
+        subject: "CSE",
+        number: 111,
+        title: "Programming with Functions",
+        credits: 2,
+        complete: true
+    },
+    {
+        subject: "CSE",
+        number: 210,
+        title: "Programming with Classes",
+        credits: 2,
+        complete: true
+    },
+    {
+        subject: "WDD",
+        number: 131,
+        title: "Dynamic Web Fundamentals",
+        credits: 2,
+        complete: true
+    },
+    {
+        subject: "WDD",
+        number: 231,
+        title: "Frontend Web Development",
+        credits: 2,
+        complete: false
+    }
+
+]
