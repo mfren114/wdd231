141bc7260fade71225be2acdd1c6e9f8d23698c5
mfren114 <mathew.frenette@yahoo.com>
2025-03-14 15:56:43 -0700

    update

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -7,31 +7,39 @@ async function getProphetData() {
     const data = await response.json();
     //console.table(data.prophets); 
     displayProphets(data.prophets);
-  }
-  
-  getProphetData();
+}
+
+getProphetData();
 
 
-  const displayProphets = (prophets) => {
+const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-      // Create elements to add to the div.cards element
-      let card = document.createElement('section');
-      let fullName = document.createElement('__'); // fill in the blank
-      let portrait = document.createElement('img');
-  
-      // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophet._____} ____________`; // fill in the blank
-      // Build the image portrait by setting all the relevant attributes
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
-  
-      // Append the section(card) with the created elements
-      card.appendChild(_______); //fill in the blank
-      card.appendChild(portrait);
-  
-      cards.appendChild(card);
-    }); // end of arrow function and forEach loop
-  }
+
+        // Create elements 
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthDate = document.createElement('p')
+        let birthPlace = document.createElement('p')
+        let portrait = document.createElement('img');
+
+        // Text content
+        fullName.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}th President`;
+        birthDate.textContent = `Born ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of birth ${prophet.birthplace}`;
+
+        // Build image portrait attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th latter-day President`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
