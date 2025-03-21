90e038363e581ea9e66cc5ee9927b5a8c24dd163
mfren114 <mathew.frenette@yahoo.com>
2025-03-21 12:19:44 -0700

    update

---
+++ weather.js
@@ -0,0 +1,26 @@
+const currentTemp = document.querySelectorAll('#current-temp');
+const weatherIcon = document.querySelectorAll('#weather-icon');
+const captionDesc = document.querySelectorAll('#figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.74}&lon={6.64}&units=${units}&appid={bf30c96291d9b301f0d9c9f998bb34b7}';
+
+async function apifetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+apifetch();
+
+function displayResults(data) {
+    
+}
