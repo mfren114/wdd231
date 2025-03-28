8631ac478a7fd69ee5f4e8bdea28c325b6a8130a
mfren114 <mathew.frenette@yahoo.com>
2025-03-28 16:39:42 -0700

    u

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,9 +1,8 @@
 var d = new Date();
 
 //Date and Time
-document.getElementById("date").value = d.toDateString();
-
+var date = d.toDateString();
 var hours = d.getHours();
 var mins = d.getMinutes();
 var seconds = d.getSeconds();
-document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+document.getElementById("date").value = date + hours + ":" + mins + ":" + seconds;
