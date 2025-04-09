7958e5ccba91816f05381057d993d02ad2cd7feb
mfren114 <mathew.frenette@yahoo.com>
2025-04-09 12:54:51 -0700

    update

--- project/data/stars.mjs
+++ project/data/stars.mjs
@@ -5,15 +5,15 @@ export const stars = [
         "jerseyNumber": "Number: 30",
         "age": "Age: 37",
         "championships": "Chips: 4",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "curry.webp"
     },
     {
         "name": "Lebron James",
-        "team": "Lakers",
+        "team": "Team: Lakers",
         "jerseyNumber": "Number: 23",
         "age": "Age: 40",
         "championships": "Chips: 4",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "ljames.webp"
     },
     {
         "name": "Luka Doncic",
@@ -21,110 +21,110 @@ export const stars = [
         "jerseyNumber": "Number: 77",
         "age": "Age: 25",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "doncic.webp"
     },
     {
         "name": "Jimmy Butler",
-        "team": "Warriors",
+        "team": "Team: Warriors",
         "jerseyNumber": "Number: 10",
         "age": "Age: 37",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "butler.webp"
     },
     {
         "name": "Kawhi Leonard",
-        "team": "Clippers",
+        "team": "Team: Clippers",
         "jerseyNumber": "Number: 2",
         "age": "Age: 32",
         "championships": "Chips: 2",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "leonard.webp"
     },
     {
         "name": "James Harden",
-        "team": "Clippers",
+        "team": "Team: Clippers",
         "jerseyNumber": "Number: 1",
         "age": "Age: 35",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "jharden.webp"
     },
     {
         "name": "Ja Morant",
-        "team": "Grizzlies",
+        "team": "Team: Grizzlies",
         "jerseyNumber": "Number: 12",
         "age": "Age: 25",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "morant.webp"
     },
     {
         "name": "Shai Gilgeous-Alexander",
-        "team": "Thunder",
+        "team": "Team: Thunder",
         "jerseyNumber": "Number: 2",
         "age": "Age: 26",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "sga.webp"
     },
     {
         "name": "Kyrie Irving",
-        "team": "Mavericks",
+        "team": "Team: Mavericks",
         "jerseyNumber": "Number: 11",
         "age": "Age: 33",
         "championships": "Chips: 1",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "irving.webp"
     },
     {
         "name": "Giannis Antetokounmpo",
-        "team": "Bucks",
+        "team": "Team: Bucks",
         "jerseyNumber": "Number: 34",
         "age": "Age: 30",
         "championships": "Chips: 1",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "giannis.webp"
     },
     {
         "name": "Jalen Brunson",
-        "team": "Knicks",
+        "team": "Team: Knicks",
         "jerseyNumber": "Number: 11",
         "age": "Age: 28",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "brunson.webp"
     },
     {
         "name": "Nikola Jokic",
-        "team": "Nuggets",
+        "team": "Team: Nuggets",
         "jerseyNumber": "Number: 15",
         "age": "Age: 30",
         "championships": "Chips: 1",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "jokic.webp"
     },
     {
         "name": "Jayson Tatum",
-        "team": "Celtics",
+        "team": "Team: Celtics",
         "jerseyNumber": "Number: 0",
         "age": "Age:27",
         "championships": "Chips: 1",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "tatum.webp"
     },
     {
         "name": "Lamelo Ball",
-        "team": "Hornets",
+        "team": "Team: Hornets",
         "jerseyNumber": "Number: 1",
         "age": "Age: 23",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "ball.webp"
     },
     {
         "name": "Kevin Durant",
-        "team": "Suns",
+        "team": "Team: Suns",
         "jerseyNumber": "Number: 35",
         "age": "Age: 36",
         "championships": "Chips: 2",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "durant.webp"
     },
     {
         "name": "Devin Booker",
-        "team": "Suns",
+        "team": "Team: Suns",
         "jerseyNumber": "Number: 1",
         "age": "Age: 28",
         "championships": "Chips: 0",
-        "image": "https://churchofjesuschrist.org/imgs/cc1bb422d3085b88ecff017177d8be41808d229f/full/1920%2C/0/default"
+        "image": "booker.webp"
     }
 ]
