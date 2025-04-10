c1c640eafc5553ef501becda3f3cac6e4738f865
mfren114 <mathew.frenette@yahoo.com>
2025-04-10 13:56:01 -0700

    update

--- project/scripts/discover.js
+++ project/scripts/discover.js
@@ -17,3 +17,183 @@ curryModal.addEventListener("click", () => {
 curryClose.addEventListener('click', () => {
     curryDialog.close();
 });
+
+const lebronModal = document.querySelector('#lebron-modal')
+const lebronDialog = document.querySelector('#lebron-dialog')
+const lebronClose = document.querySelector('#lebron-close')
+
+lebronModal.addEventListener("click", () => {
+    lebronDialog.showModal();
+});
+
+lebronClose.addEventListener('click', () => {
+    lebronDialog.close();
+});
+
+const ballModal = document.querySelector('#ball-modal')
+const ballDialog = document.querySelector('#ball-dialog')
+const ballClose = document.querySelector('#ball-close')
+
+ballModal.addEventListener("click", () => {
+    ballDialog.showModal();
+});
+
+ballClose.addEventListener('click', () => {
+    ballDialog.close();
+});
+
+const bookerModal = document.querySelector('#booker-modal')
+const bookerDialog = document.querySelector('#booker-dialog')
+const bookerClose = document.querySelector('#booker-close')
+
+bookerModal.addEventListener("click", () => {
+    bookerDialog.showModal();
+});
+
+bookerClose.addEventListener('click', () => {
+    bookerDialog.close();
+});
+
+const brunsonModal = document.querySelector('#brunson-modal')
+const brunsonDialog = document.querySelector('#brunson-dialog')
+const brunsonClose = document.querySelector('#brunson-close')
+
+brunsonModal.addEventListener("click", () => {
+    brunsonDialog.showModal();
+});
+
+brunsonClose.addEventListener('click', () => {
+    brunsonDialog.close();
+});
+
+const butlerModal = document.querySelector('#butler-modal')
+const butlerDialog = document.querySelector('#butler-dialog')
+const butlerClose = document.querySelector('#butler-close')
+
+butlerModal.addEventListener("click", () => {
+    butlerDialog.showModal();
+});
+
+butlerClose.addEventListener('click', () => {
+    butlerDialog.close();
+});
+
+const doncicModal = document.querySelector('#doncic-modal')
+const doncicDialog = document.querySelector('#doncic-dialog')
+const doncicClose = document.querySelector('#doncic-close')
+
+doncicModal.addEventListener("click", () => {
+    doncicDialog.showModal();
+});
+
+doncicClose.addEventListener('click', () => {
+    doncicDialog.close();
+});
+
+const durantModal = document.querySelector('#durant-modal')
+const durantDialog = document.querySelector('#durant-dialog')
+const durantClose = document.querySelector('#durant-close')
+
+durantModal.addEventListener("click", () => {
+    durantDialog.showModal();
+});
+
+durantClose.addEventListener('click', () => {
+    durantDialog.close();
+});
+
+const irvingModal = document.querySelector('#irving-modal')
+const irvingDialog = document.querySelector('#irving-dialog')
+const irvingClose = document.querySelector('#irving-close')
+
+irvingModal.addEventListener("click", () => {
+    irvingDialog.showModal();
+});
+
+curryClose.addEventListener('click', () => {
+    curryDialog.close();
+});
+
+const giannisModal = document.querySelector('#giannis-modal')
+const giannisDialog = document.querySelector('#giannis-dialog')
+const giannisClose = document.querySelector('#giannis-close')
+
+giannisModal.addEventListener("click", () => {
+    giannisDialog.showModal();
+});
+
+giannisClose.addEventListener('click', () => {
+    giannisDialog.close();
+});
+
+const hardenModal = document.querySelector('#harden-modal')
+const hardenDialog = document.querySelector('#harden-dialog')
+const hardenClose = document.querySelector('#harden-close')
+
+hardenModal.addEventListener("click", () => {
+    hardenDialog.showModal();
+});
+
+hardenClose.addEventListener('click', () => {
+    hardenDialog.close();
+});
+
+const jokicModal = document.querySelector('#jokic-modal')
+const jokicDialog = document.querySelector('#jokic-dialog')
+const jokicClose = document.querySelector('#jokic-close')
+
+jokicModal.addEventListener("click", () => {
+    jokicDialog.showModal();
+});
+
+jokicClose.addEventListener('click', () => {
+    jokicDialog.close();
+});
+
+const leonardModal = document.querySelector('#leonard-modal')
+const leonardDialog = document.querySelector('#leonard-dialog')
+const leonardClose = document.querySelector('#leonard-close')
+
+leonardModal.addEventListener("click", () => {
+    leonardDialog.showModal();
+});
+
+leonardClose.addEventListener('click', () => {
+    leonardDialog.close();
+});
+
+const morantModal = document.querySelector('#morant-modal')
+const morantDialog = document.querySelector('#morant-dialog')
+const morantClose = document.querySelector('#morant-close')
+
+morantModal.addEventListener("click", () => {
+    morantDialog.showModal();
+});
+
+morantClose.addEventListener('click', () => {
+    morantDialog.close();
+});
+
+const tatumModal = document.querySelector('#tatum-modal')
+const tatumDialog = document.querySelector('#tatum-dialog')
+const tatumClose = document.querySelector('#tatum-close')
+
+tatumModal.addEventListener("click", () => {
+    tatumDialog.showModal();
+});
+
+tatumClose.addEventListener('click', () => {
+    tatumDialog.close();
+});
+
+const sgaModal = document.querySelector('#sga-modal')
+const sgaDialog = document.querySelector('#sga-dialog')
+const sgaClose = document.querySelector('#sga-close')
+
+sgaModal.addEventListener("click", () => {
+    sgaDialog.showModal();
+});
+
+sgaClose.addEventListener('click', () => {
+    sgaDialog.close();
+});
