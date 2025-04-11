b2f3789be072b412d700a2a5ed5b9998ef8428f3
mfren114 <mathew.frenette@yahoo.com>
2025-04-11 10:47:08 -0700

    update

--- project/scripts/stars.js
+++ project/scripts/stars.js
@@ -38,16 +38,6 @@ function displayItems(stars) {
 
 displayItems(stars)
 
-//hamburger button
-
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function () {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
-
 //weather
 
 const currentTemp = document.querySelector('#current-temp');
